efcbace3ec5a24ba13060c3e1c87f6b8a6b29959
mahdiqezlou <mahdiqezlou@gmail.com>
2022-02-17 22:20:44 -0800

    Arbitray ranks for FGPA - Now I can run FGPA even on a pc with mem ~ 15 GB

--- src/lytomo_watershed/fgpa.py
+++ src/lytomo_watershed/fgpa.py
@@ -46,9 +46,9 @@ def get_sample_spectra(MPI, z, num, seed=13, savedir='density_highres/',
     # Initialize the MPI communication
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
-    tau_conv, xrank, yrank = get_tau_conv(comm=comm, z=z, savedir=savedir,
-                                          boxsize=boxsize, Ngrids=Ngrids,
-                                          SmLD=SmLD, SmLV=SmLV)
+    tau_conv = get_tau_conv(comm=comm, z=z, savedir=savedir,
+                            boxsize=boxsize, Ngrids=Ngrids,
+                            SmLD=SmLD, SmLV=SmLV)
     
     if seed is not None:
         cofm =get_cofm(seed=seed, num=num, boxsize= tau_conv.shape[0]).astype(int)
@@ -58,11 +58,10 @@ def get_sample_spectra(MPI, z, num, seed=13, savedir='density_highres/',
         cofm[:,0] = np.ravel(x)
         cofm[:,1] = np.ravel(y)
         del x, y
-    
+    ind = np.where(tau_conv!= -1)
     tau_sampled = np.zeros(shape=(cofm.shape[0], tau_conv.shape[2]))
-    xrank, yrank = np.arange(xrank[0], xrank[1]+1,1), np.arange(yrank[0], yrank[1]+1,1)
     # Find the sample spectra on this rank
-    ind_cofm = np.where(np.isin(cofm[:,0], xrank)*np.isin(cofm[:,1], yrank))[0]
+    ind_cofm = np.where(np.isin(cofm[:,0], ind[0])*np.isin(cofm[:,1], ind[1]))[0]
     tau_sampled[ind_cofm] = tau_conv[cofm[ind_cofm,0], cofm[ind_cofm,1],:]
     ### MPI part
     # Make sure the data is contiguous in memeory
@@ -126,17 +125,16 @@ def get_noiseless_map(MPI, z, savedir='density_highres/', savefile='FGPA_flux_z2
     # MPI communications
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
-    tau_conv, xrank, yrank = get_tau_conv(comm=comm, z=z, savedir=savedir, boxsize=boxsize, Ngrids=Ngrids, SmLD=SmLD, SmLV=SmLV)
-    
-    # Fix the mean flux
-    # This may not be accurate since mean flux on each data chunck is corrected seperately
-    if fix_mean_flux and (xrank!=0) and (yrank!=0):
+    tau_conv = get_tau_conv(comm=comm, z=z, savedir=savedir, boxsize=boxsize, Ngrids=Ngrids, SmLD=SmLD, SmLV=SmLV)
+    ind = tau_conv != -1
+    if fix_mean_flux:
         if mean_flux is None:
             mean_flux = sm.get_mean_flux(z=z)
         print('mean flux is ', mean_flux, flush=True)
-        scale = fs.mean_flux(tau_conv[xrank[0]:xrank[1]+1, yrank[0]:yrank[1]+1,:], mean_flux_desired=mean_flux)
+        scale = fs.mean_flux(tau_conv[ind], mean_flux_desired=mean_flux)
     else :
         scale = 1
+    tau_conv[~ind] = 0
     ### Resampling pixels along spectra
     flux_conv = resample_flux(scale*tau_conv, Npix)
 
@@ -155,7 +153,7 @@ def get_noiseless_map(MPI, z, savedir='density_highres/', savefile='FGPA_flux_z2
 
 
 def resample_flux(tau, Npix):
-    """Resample spectra to get Npix pixels along line of sight. It is done by averaging over
+    """Resample spectra to get Npix pixels along line of sight. It is done by averaging the flux over
     few consecutive pixels.
     tau : Optical depth.
     Npix : The desired number of pxiels 
@@ -167,8 +165,7 @@ def resample_flux(tau, Npix):
     for t in range(Npix):
         flux[:,:,t] = np.sum(np.exp(-tau[:,:,t*addpix:(t+1)*addpix]), axis=2)/addpix 
     flux = gaussian_filter1d(flux, sigma=1, mode='wrap')
-    return flux
-
+    return flux 
 
 def get_tau_conv(comm, z, savedir='density_highres/', savefile='FGPA_flux_z2.4.hdf5',boxsize=205, Ngrids=205, SmLD=1, SmLV=1):
     """ Calculate tau in redshift space
@@ -182,71 +179,83 @@ def get_tau_conv(comm, z, savedir='density_highres/', savefile='FGPA_flux_z2.4.h
         tau_conv : convoluted optical depth
         [xstart, xend], [ystart, yend] : the ranges of x and y coordinates covered by any rank
         """
-
+    import glob
+    import os
+    from . import mpi4py_helper
+    
     rank = comm.Get_rank()
     size = comm.Get_size()
-    with h5py.File(savedir+str(rank)+'_densfield.hdf5','r') as f:
-        # nbodykit does not break the data along the z direction, so Nz is the 
-        # the size of the initial density map in all 3 dimentions
-        Nz = f['DM/dens'][:].shape[2]
-        dvbin = cosmo.H(z).value*boxsize/(cosmo.h*Nz*(1+z))
-        up = np.arange(Nz)*dvbin
-        tau_conv = np.zeros(shape=(Ngrids, Ngrids, Nz))
-        # Approx position of the desired sightlines. The approximation should be ok
-        # for FGPA since the density map has very fine voxels
-        x, y = int(Nz/Ngrids)*np.arange(Ngrids), int(Nz/Ngrids)*np.arange(Ngrids)
-        # Which sightlines are on this rank
-        indx = np.where(np.isin(x, f['DM/x'][:]))[0]
-        if indx.size == 0:
-            # Some ranks may not hold any sightlines at all
-            print('The sightline coordinates are not on density grids on rank=', 
-                  rank, flush=True)
-            print("The y desnity grid coordinates are = ", f['DM/y'][:], flush=True)
-            return tau_conv, [0,0], [0,0]
-        
-        xstart, xend = indx[0], indx[-1]
-        indy = np.where(np.isin(y, f['DM/y'][:]))[0]
-        if indy.size == 0:
-            # Some ranks may not hold any sightlines at all
-            print('The sightline coordinates are not on density grids on rank=', 
-                  rank, flush=True)
-            print("The y desnity grid coordinates are = ", f['DM/y'][:], flush=True)
-            return tau_conv, [0,0], [0,0]
-        
-        ystart, yend = indy[0], indy[-1]
-        print('Sightlines on Rank =', rank, (int(xstart), int(xend)), (int(ystart), int(yend)) ,flush=True)
-        # i, j are indices for the final flux map (Ngrids * Ngrids)
-        for i in range(xstart, xend+1):
-            if rank ==0:
-                print(str(int(100*(i-xstart)/indx.size))+'%', flush=True )
-            # Indices on f['DM/dens'] map
-            ic = x[i] - f['DM/x'][0]
-            for j in range(ystart, yend+1):
+    fnames = glob.glob(os.path.join(savedir,'*_densfield.hdf5'))
+    fnames = mpi4py_helper.distribute_files(comm=comm, fnames=fnames)
+    
+    tau_conv = None
+    c=0
+    for fn in fnames:
+        c+=1
+        print(rank, fn, flush=True)
+        with h5py.File(fn,'r') as f:
+            if tau_conv is None:
+                # nbodykit does not break the data along the z direction, so Nz is the 
+                # the size of the initial density map in all 3 dimentions
+                Nz = f['DM/dens'][:].shape[2]
+                dvbin = cosmo.H(z).value*boxsize/(cosmo.h*Nz*(1+z))
+                up = np.arange(Nz)*dvbin
+                tau_conv = -1*np.ones(shape=(Ngrids, Ngrids, Nz))
+                # Approx position of the desired sightlines. The approximation should be ok
+                # for FGPA since the density map has very fine voxels
+                x, y = int(Nz/Ngrids)*np.arange(Ngrids), int(Nz/Ngrids)*np.arange(Ngrids)
+            # Which sightlines are on this rank
+            indx = np.where(np.isin(x, f['DM/x'][:]))[0]
+            if indx.size == 0:
+                # Some ranks may not hold any sightlines at all
+                print('The sightline coordinates are not on density grids on rank=', 
+                      rank, flush=True)
+                print("The y desnity grid coordinates are = ", f['DM/y'][:], flush=True)
+                return tau_conv, [0,0], [0,0]
+
+            xstart, xend = indx[0], indx[-1]
+            indy = np.where(np.isin(y, f['DM/y'][:]))[0]
+            if indy.size == 0:
+                # Some ranks may not hold any sightlines at all
+                print('The sightline coordinates are not on density grids on rank=', 
+                      rank, flush=True)
+                print("The y desnity grid coordinates are = ", f['DM/y'][:], flush=True)
+                return tau_conv, [0,0], [0,0]
+
+            ystart, yend = indy[0], indy[-1]
+            print('Sightlines on Rank =', rank, (int(xstart), int(xend)), (int(ystart), int(yend)) ,flush=True)
+            # i, j are indices for the final flux map (Ngrids * Ngrids)
+            for i in range(xstart, xend+1):
+                if rank ==1:
+                    print(str(int(100*c/len(fnames)))+'%', flush=True )
                 # Indices on f['DM/dens'] map
-                jc = y[j] - f['DM/y'][0]
-                dens = f['DM/dens'][ic,jc,:]
-                tau_real = get_tau_real(f['DM/dens'][ic,jc,:], z=z)
-                # Peculiar velocity addition
-                ind = np.where((dens != 0))
-                vel_pec = np.zeros_like(f['DM/pz'][ic,jc,:])
-                vel_pec[ind] = f['DM/pz'][ic,jc,:][ind]/(dens[ind]*np.sqrt(1+z))
-                vel_pec = gaussian_filter1d(vel_pec, SmLV)
-                dens = gaussian_filter1d(dens, SmLD)
-                u0 = up + vel_pec
-                btherm = get_btherm(dens)
-                # To avoide devision by 0, if b_threm == 0, pass a nonzero value since
-                # tau_real is 0 in that voxel anyway, tau_conv will be 0.
-                btherm[np.where(btherm==0)] = 1.0
-                for k in range(Nz):
-                    dvel = np.abs(up[k]-u0)
-                    # Periodic Boundary
-                    indv = np.where(dvel > dvbin*Nz/2)
-                    dvel[indv] = dvbin*Nz - dvel[indv]
-                    Voight = (1/btherm)*np.exp(-(dvel/btherm)**2)
-                    tau_conv[i,j,k] = np.sum(tau_real*Voight*dvbin)
-        comm.Barrier()
-        print('Rank', rank, 'is done with tau_conv', flush=True)
-        return tau_conv, [xstart,xend], [ystart,yend]
+                ic = x[i] - f['DM/x'][0]
+                for j in range(ystart, yend+1):
+                    # Indices on f['DM/dens'] map
+                    jc = y[j] - f['DM/y'][0]
+                    dens = f['DM/dens'][ic,jc,:]
+                    tau_real = get_tau_real(f['DM/dens'][ic,jc,:], z=z)
+                    # Peculiar velocity addition
+                    ind = np.where((dens != 0))
+                    vel_pec = np.zeros_like(f['DM/pz'][ic,jc,:])
+                    vel_pec[ind] = f['DM/pz'][ic,jc,:][ind]/(dens[ind]*np.sqrt(1+z))
+                    vel_pec = gaussian_filter1d(vel_pec, SmLV)
+                    dens = gaussian_filter1d(dens, SmLD)
+                    u0 = up + vel_pec
+                    btherm = get_btherm(dens)
+                    # To avoide devision by 0, if b_threm == 0, pass a nonzero value since
+                    # tau_real is 0 in that voxel anyway, tau_conv will be 0.
+                    btherm[np.where(btherm==0)] = 1.0
+                    for k in range(Nz):
+                        dvel = np.abs(up[k]-u0)
+                        # Periodic Boundary
+                        indv = np.where(dvel > dvbin*Nz/2)
+                        dvel[indv] = dvbin*Nz - dvel[indv]
+                        Voight = (1/btherm)*np.exp(-(dvel/btherm)**2)
+                        tau_conv[i,j,k] = np.sum(tau_real*Voight*dvbin)
+    comm.Barrier()
+    print('Rank', rank, 'is done with tau_conv', flush=True)
+    return tau_conv
     
 def get_tau_real(Delta, z):
     """ Get tau in real space
